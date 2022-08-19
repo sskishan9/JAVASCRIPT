25dd67909f882908b458426943c40bb1e8fecbb6
sskishan9 <sameerkishan99@gmail.com>
2022-08-19 10:59:10 +0530

    modified

--- first.js
+++ first.js
@@ -1,3 +1,4 @@
+//Values & Variables
 let js='amazing';
 if(js === 'amazing') {
     alert('Javascript is Fun!!!');
@@ -6,3 +7,11 @@ if(js === 'amazing') {
 console.log(40+8+13-24+31);
 let firstname="sameer";
 console.log(firstname);
+
+//DataTypes
+let javaScriptIsFun=true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Sameer');
+console.log(typeof false);
