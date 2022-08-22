98e8a49e918901ddb69233f5c3782fcaf81e6034
sskishan9 <sameerkishan99@gmail.com>
2022-08-22 11:50:08 +0530

    init

--- first.js
+++ first.js
@@ -1,4 +1,6 @@
 //Values & Variables
+
+
 let js='amazing';
 if(js === 'amazing') {
     alert('Javascript is Fun!!!');
@@ -15,3 +17,26 @@ console.log(typeof javaScriptIsFun);
 console.log(typeof 23);
 console.log(typeof 'Sameer');
 console.log(typeof false);
+
+//Declaring Variables
+
+//variables can be reassigned by using let keyword.
+//Const variables cannot be muted and should be assigned to some value when declaring.
+//var and let will work similar 
+let age=30;
+age=31;
+
+//Operators
+const ageJonas=2037-1991;
+const ageSarah=2037-2018;
+console.log(ageJonas,ageSarah);
+console.log(ageJonas*2,ageJonas/10,2**3);
+
+const fname='Sameer';
+const lname="kishan";
+console.log(fname+" "+lname);
+console.log(ageJonas>ageSarah);
+
+
+
+
