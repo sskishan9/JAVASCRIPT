54365901adde7d12ce823db5c104457ee96ebb13
sskishan9 <sameerkishan99@gmail.com>
2022-09-12 16:04:14 +0530

    init

---
+++ Prompt.js
@@ -0,0 +1,2 @@
+const name=prompt('Enter your name','Sameer');
+alert(`Your name is ${name}`);
--- first.js
+++ first.js
@@ -1,4 +1,4 @@
-//Values & Variables
+Values & Variables
 
 
 let js='amazing';
@@ -46,6 +46,38 @@ let bmi1=markWeight/(markHeight**2);
 let bmi2=johnWeight/(johnHeight**2);
 let markHigherBmi=bmi1>bmi2;
 console.log(bmi1,bmi2,markHigherBmi);
+
+//String and Template Literals 
+const firstname='Sameer Kishan';
+const job='teaching';
+const birthyear=2000;
+const year =2022;
+const res="I'm " + firstname +" of " + (year-birthyear) + " years old and my profession is " + job;
+console.log(res);
+
+//Asignment2
+let markWeight=78;
+let markHeight=1.69;
+let johnWeight=92;
+let johnHeight=1.95;
+let bmi1=markWeight/(markHeight**2);
+let bmi2=johnWeight/(johnHeight**2);
+if(bmi1>bmi2){
+    console.log(`Mark has Higher BMI`);
+}
+else 
+{
+    console.log(`John Has Higher BMI`);
+}
+
+// === is strictly equal == is loosely equal
+
+//ternary operator 
+const bill=275;
+const tip=bill<=300 && bill>=50 ? bill*0.15 : bill*0.20;
+console.log(bill+tip);
+
+
 
 
 
