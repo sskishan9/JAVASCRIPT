96dfe76b9df3c3a3f84bc91a5fc4951deedd21a0
sskishan9 <sameerkishan99@gmail.com>
2022-09-15 14:30:01 +0530

    init

--- Array.js
+++ Array.js
@@ -9,3 +9,31 @@ console.log(players);
 //pop() is the method to remove last element from Array
 players.pop();
 console.log(players);
+
+for(let count=10;count<=100;count+=10)
+{
+    console.log(count);
+}
+
+for(let i=0;i<players.length;i++)
+{
+    console.log(players[i]);
+}
+
+function rollDice()
+{
+    let n=Math.floor(Math.random()*6)+1;
+    console.log(n);
+}
+rollDice();
+
+let hasSolvedChallenge=false;
+let hasHintsLeft=false;
+if(!hasSolvedChallenge && !hasHintsLeft)
+{
+    showSolution();
+}
+function showSolution()
+{
+    console.log('Solution is Generated');
+}
--- BlackJack.js
+++ BlackJack.js
@@ -1,15 +1,32 @@
-let firstCard=10;
-let secondCard=4;
+let firstCard=getRandomCard();
+let secondCard=getRandomCard();
 let cards=[firstCard,secondCard];
 let sum=firstCard+secondCard;
 let hasBlackJack=false;
-let isAlive=true;
+let isAlive=false;
 let msg=""
 let messageEl=document.getElementById('message-el');
 let sumEl=document.getElementById('sum-el');
 let cardsEl=document.querySelector('#cards-el');
-function startGame()
+
+function getRandomCard()
 {
+    let n=Math.floor(Math.random()*13)+1;
+    if(n===1)
+    {
+        return 11;
+    }
+    else if(n>10)
+    {
+        return 10;
+    }
+    else 
+    {
+        return n;
+    }
+}
+function startGame()
+{   isAlive=true;
     renderGame();
 }
 function renderGame()
@@ -29,16 +46,26 @@ function renderGame()
         isAlive=false;
     }
     messageEl.textContent=msg;
-    cardsEl.textContent='Cards are'+' '+cards[0]+' and '+cards[1];
+    cardsEl.textContent='Cards are';
+    for(let i=0;i<cards.length;i++)
+    {
+        cardsEl.textContent+=' '+cards[i];
+    }
     sumEl.textContent="Sum : "+sum;
     
 }
 
 function newCard()
 {
+    if(isAlive && hasBlackJack)
+    {
     console.log('Drawing a new card from Deck');
-    let card=6;
+    let card=getRandomCard();
     sum+=card;
+    cards.push(card);
+    console.log(cards);
     renderGame();
+    }
+    
 }
 
